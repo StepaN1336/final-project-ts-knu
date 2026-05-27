4da9e73b3c0717af1f807f705ccdc645dd4c945d
Степан Сигіль <ssigilstopa@gmail.com>
2026-05-27 10:55:23 +0200

    make static build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change 'your-repo-name' to the actual GitHub repo name before deploying
-// e.g. base: '/smart-trip-planner/'
 export default defineConfig({
   plugins: [react()],
-  base: '/smart-trip-planner/',  // ← змінити на назву репозиторію
+  base: '/final-project-ts-knu/',
 })
